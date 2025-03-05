c411f757e74d292c23d21d1f4387ca49f2b8ea79
sailakshmy <sailakshmy94@gmail.com>
2025-03-05 20:42:28 +0400

    Updated my skills

--- src/pages/skills/Skills.js
+++ src/pages/skills/Skills.js
@@ -1,93 +1,114 @@
-export const skills={
-    frontend:[
-        {
-            skillName: "JavaScript",
-            skillLevel: 80
-        },
-        {
-            skillName: "React",
-            skillLevel: 75
-        },
-        {
-            skillName: "Redux",
-            skillLevel: 60
-        },
-        {
-            skillName: "HTML",
-            skillLevel: 60
-        },
-        {
-            skillName: "Bootstrap",
-            skillLevel: 40
-        },
-        {
-            skillName: "CSS",
-            skillLevel: 40
-        },
-        {
-            skillName: "TypeScript",
-            skillLevel: 30
-        },
-        
-    ],
-    backend:[
-        {
-            skillName: "Node",
-            skillLevel: 30
-        },
-        {
-            skillName: "Express",
-            skillLevel: 30
-        }
-    ],
-    db:[
-        {
-            skillName: "Oracle 11G",
-            skillLevel: 45
-        },
-        {
-            skillName: "SQL Developer",
-            skillLevel: 45
-        },
-        {
-            skillName: "MongoDB",
-            skillLevel: 30
-        },
-    ],
-    cloudServices:[
-        {
-            skillName: "Oracle Cloud Infrastructure",
-            skillLevel: 20
-        },
-        {
-            skillName: "Google Firebase",
-            skillLevel: 15
-        },
-    ],
-    ordercareManagement:[
-        {
-            skillName: "Ericsson ConceptWave",
-            skillLevel: 60
-        },
-    ],
-    versionControl:[
-        {
-            skillName: "git",
-            skillLevel: 50
-        },
-        {
-            skillName: "svn",
-            skillLevel: 30
-        },
-    ],
-    projectManagementTools:[
-        {
-            skillName: "Jira",
-            skillLevel: 70
-        },
-        {
-            skillName: "Confluence",
-            skillLevel: 50
-        },
-    ]
-}
+export const skills = {
+  frontend: [
+    {
+      skillName: "JavaScript",
+      skillLevel: 80,
+    },
+    {
+      skillName: "React",
+      skillLevel: 75,
+    },
+    {
+      skillName: "NextJs",
+      skillLevel: 60,
+    },
+    {
+      skillName: "Redux",
+      skillLevel: 60,
+    },
+    {
+      skillName: "HTML",
+      skillLevel: 60,
+    },
+    {
+      skillName: "Tailwind CSS",
+      skillLevel: 50,
+    },
+    {
+      skillName: "Bootstrap",
+      skillLevel: 40,
+    },
+    {
+      skillName: "CSS",
+      skillLevel: 40,
+    },
+    {
+      skillName: "TypeScript",
+      skillLevel: 40,
+    },
+  ],
+  backend: [
+    {
+      skillName: "Node",
+      skillLevel: 30,
+    },
+    {
+      skillName: "Express",
+      skillLevel: 30,
+    },
+    {
+      skillName: "GraphQL",
+      skillLevel: 30,
+    },
+  ],
+  db: [
+    {
+      skillName: "Oracle 11G",
+      skillLevel: 45,
+    },
+    {
+      skillName: "SQL Developer",
+      skillLevel: 45,
+    },
+    {
+      skillName: "MongoDB",
+      skillLevel: 30,
+    },
+  ],
+  testDrivenDevelopment: [
+    {
+      skillName: "React Testing Library",
+      skillLevel: 60,
+    },
+    {
+      skillName: "Jest",
+      skillLevel: 60,
+    },
+  ],
+  cloudServices: [
+    {
+      skillName: "Oracle Cloud Infrastructure",
+      skillLevel: 20,
+    },
+    {
+      skillName: "Google Firebase",
+      skillLevel: 15,
+    },
+  ],
+  ordercareManagement: [
+    {
+      skillName: "Ericsson ConceptWave",
+      skillLevel: 60,
+    },
+  ],
+  versionControl: [
+    {
+      skillName: "git",
+      skillLevel: 50,
+    },
+    {
+      skillName: "svn",
+      skillLevel: 30,
+    },
+  ],
+  projectManagementTools: [
+    {
+      skillName: "Jira",
+      skillLevel: 70,
+    },
+    {
+      skillName: "Confluence",
+      skillLevel: 50,
+    },
+  ],
+};
